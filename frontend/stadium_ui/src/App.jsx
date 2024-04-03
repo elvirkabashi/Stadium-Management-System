22094d6f674562c77d9606cdfb579d74e421e2cf
elvirkabashi <74553516+elvirkabashi@users.noreply.github.com>
2024-04-03 15:14:23 +0200

    Pjesa userit dhe adminit

--- frontend/stadium_ui/src/App.jsx
+++ frontend/stadium_ui/src/App.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
+import { useState } from 'react'
 import './App.css'
-import { Routes, Route, BrowserRouter, Navigate} from 'react-router-dom';
+import { Routes, Route, BrowserRouter} from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import Sidebar from './components/Sidebar';
 
 const App = () => {
+                              //provo true ose false
+  const [isAdmin,setIsAdmin] = useState(false)//Nese admini eshte true kan me u shfaq Sidebari dhe Routes per adminin 
+                                            //ne te kunderten ka mu shfaq pjesa e userit
+
+                                            //BrowserRouter per user te tjesht kan mu caktu te app.jsx 
+                                            //kurse per pjesen e adminit BrowserRouter jan te file Sidebar.jsx
+
   return (
-    <BrowserRouter>
+    <>
+    {isAdmin ? (
+      <Sidebar /> 
+    ) : (
+      <BrowserRouter>
         <Routes>
           <Route path='/' element={<HomePage />} />
-          <Route path='/dashboard' element={<Sidebar />} />
         </Routes>
-  </BrowserRouter>
+      </BrowserRouter>
+    )}
+  </>
   )
 }
 
