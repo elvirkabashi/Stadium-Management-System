55aa803fd5a56b733796d786aa12c23cd3a50208
artindervishii <ad58448@ubt-uni.net>
2024-04-05 03:37:02 +0200

    Konfigurimi  i mongodb in progress

--- backend/StadiumMicroserviceSolution/MongoDbWebApi/Program.cs
+++ backend/StadiumMicroserviceSolution/MongoDbWebApi/Program.cs
@@ -1,3 +1,7 @@
+using MongoDB.Driver;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+IConfiguration configuration = new ConfigurationBuilder()
+    .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+var mongoclient = new MongoClient(configuration.GetConnectionString("MongoDb"));
+builder.Services.AddSingleton<MongoClient>(mongoclient);
+//builder.Services.AddTransient<MongoDbWebApiContext>();
+
 
 var app = builder.Build();
 
