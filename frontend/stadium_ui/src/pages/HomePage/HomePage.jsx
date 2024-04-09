ea3de3aa917a549bff1d3ef4198ab8f97669589c
Rigon Pira <rp57421@ubt-uni.net>
2024-04-09 03:31:01 +0200

    Implementimi i pjeses se slider

--- frontend/stadium_ui/src/pages/HomePage/HomePage.jsx
+++ frontend/stadium_ui/src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,14 @@
 import '../../App.css'
 import Home from '../Home/Home';
 import ShortcutMain from '../HomePage/ShortcutMain/ShortcutMain';
+import Homeslider from '../Slider/homeslider/slider';
 import Map from './Map/Map';
 
 
 const HomePage = () => {
   return (
     <>
+    <Homeslider />
     <ShortcutMain />
     <Home />
     <Map />
---
+++ frontend/stadium_ui/src/pages/Slider/homeslider/slider.jsx
@@ -0,0 +1,96 @@
+
+import React from 'react'
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+// import Swiper core and required modules
+import { Navigation, Autoplay} from 'swiper/modules';
+// import {StaticImage} from "gatsby-plugin-image"
+import kosovabanner from './images/kosovabanner.png'
+import ffk from './images/ffk.png'
+import firstmatch from './images/firstmatch.png'
+import kstadium from './images/kstadium.png'
+import 'swiper/css/navigation';
+import 'swiper/css/autoplay'
+import {HiOutlineArrowNarrowLeft} from 'react-icons/hi'
+import {HiOutlineArrowNarrowRight} from 'react-icons/hi'
+import '../../../tail.css'
+import './homeslider.css'
+
+export default function Homeslider(){
+    return (
+    <div>
+        <Swiper
+        className='relative group'
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation = {{
+            nextEl:".button-next-slide", 
+            prevEl:".button-prev-slide", 
+        }}
+        autoplay={{delay:1400}}
+        modules={[Navigation, Autoplay]}
+        >
+        <SwiperSlide>
+            <div class="relative">
+                <img src={ffk} alt="" class="slide-img"/>
+                <div class="slide-content">
+                    <h3 class="slide-subtitle">DARDANET</h3>
+                    <h3 class="slide-title">KOSOVA STADIUM</h3>
+                    <p class="slide-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem nam voluptatum, error alias magnam delectus, incidunt, corporis suscipit hic minima nisi itaque quis qui harum praesentium fugit impedit beatae sint.</p>
+                    <button class="slide-button">LEXO ME SHUME</button>
+                </div>
+            </div>
+        </SwiperSlide>
+
+
+        <SwiperSlide>
+            <div class="relative">
+                <img src={kstadium} alt="" class="slide-img"/>
+                <div class="slide-content" style={{color:'white'}}>
+                    <h3 class="slide-subtitle">Stadiumi </h3>
+                    <h3 class="slide-title">KOSOVA</h3>
+                    <p class="slide-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem nam voluptatum, error alias magnam delectus, incidunt, corporis suscipit hic minima nisi itaque quis qui harum praesentium fugit impedit beatae sint.</p>
+                    <button class="slide-button">LEXO ME SHUME</button>
+                </div>
+            </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+            <div class="relative">
+                <img src={kosovabanner} alt="" class="slide-img"/>
+                <div class="slide-content" style={{color:'white'}}>
+                    <h3 class="slide-subtitle">Vikendi po sjell lojen me te mire</h3>
+                    <h2 class="slide-title">Kosova vs Romania</h2>
+                    <button class="slide-button">LEXO ME SHUME</button>
+                </div>
+            </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+            <div class="relative">
+                <img src={firstmatch} alt="" class="slide-img"/>
+                <div class="slide-content" style={{color:'black'}} id='slideBox'>
+                    <h3 class="slide-subtitle">Ndeshja e pare historike</h3>
+                    <h2 class="slide-title">Kosova vs Haiti</h2>
+                    <button class="slide-button" id='button' >LEXO ME SHUME</button>
+                </div>
+            </div>
+        </SwiperSlide>
+        
+        <div class="button-slide button-prev-slide group-hover:left-0">
+            <HiOutlineArrowNarrowLeft/>
+        </div>
+        <div class="button-slide button-next-slide group-hover:right-0">
+            <HiOutlineArrowNarrowRight/>
+        </div>
+
+
+        </Swiper>
+    </div>
+    )
+}
+
+
+
