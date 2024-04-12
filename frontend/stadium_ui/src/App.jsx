5989b24dd9dc08dcf1376f7543060e9456b92759
Rigon Pira <rp57421@ubt-uni.net>
2024-04-12 22:30:08 +0200

    Validimi i email-it tek footer-i

--- frontend/stadium_ui/src/App.jsx
+++ frontend/stadium_ui/src/App.jsx
@@ -11,7 +11,7 @@ import Stadiumi from './pages/Stadiumi/Stadiumi';
 
 const App = () => {
                               //provo true ose false
-  const [isAdmin,] = useState(true)//Nese admini eshte true kan me u shfaq Sidebari dhe Routes per adminin 
+  const [isAdmin,] = useState(false)//Nese admini eshte true kan me u shfaq Sidebari dhe Routes per adminin 
                                             //ne te kunderten ka mu shfaq pjesa e userit
 
                                             //BrowserRouter per user te tjesht kan mu caktu te app.jsx 
