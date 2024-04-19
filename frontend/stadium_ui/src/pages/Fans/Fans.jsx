7775037bf7caf95fd73664f9f6814041825d7ced
Rigon Pira <rp57421@ubt-uni.net>
2024-04-19 17:25:03 +0200

    Krijimi i stilizimit per pjesen Fans

--- frontend/stadium_ui/src/pages/Fans/Fans.jsx
+++ frontend/stadium_ui/src/pages/Fans/Fans.jsx
@@ -5,12 +5,11 @@ import {HiOutlineClipboardCheck} from 'react-icons/hi'
 import Footer from '../Footer/Footer'
 import { Link } from "react-router-dom";
 
-
 //import the images 
 import KosovoStandard from '../../Assets/KosovoStandard.jpg'
 import KosovoPremium from '../../Assets/KosovoPremium.jpg'
 import KosovoJunior from '../../Assets/KosovoJunior.jpg'
-
+import './Fans.css';
 
 import Aos from 'aos'
 import 'aos/dist/aos.css'
@@ -66,14 +65,58 @@ const Fans = () => {
 <div class="events">
           <h2 data-aos='fade-right'>FANS</h2>
         </div>
-    <section className='main container section'>
+    <section className='new-main container section'>
 
-      <div className="secTitle">
+      <div className="new-secTitle">
         <h3 data-aos="fade-right"  className="title">
           BASHKOHU ME KOMBETAREN
         </h3>
       </div>
+      <div className="new-secContent grid">
+        
+        {
+          Data.map(({id, imgSrc, destTitle, location, grade, fees, description})=>{
+            return( 
+              <div key={id} className="singleDestination" data-aos="fade-up">
+                
+                <div className="imageDiv">
+                  <img src={imgSrc} alt={destTitle} />
+                </div>
 
+                <div className="cardInfo">
+                  <h4 className="destTitle">{destTitle}</h4>
+                  <span className="continent flex">
+                     <span className="name">{location}</span>
+                  </span>
+
+                  <div className="fees flex">
+                    <div className="grade">
+                      <span>{grade}</span>
+                    </div>
+                    <div className="price">
+                      <span>{fees}</span>
+                    </div>
+ 
+                  </div>
+
+                  <div className="desc">
+                    <p>{description}</p>
+                  </div>
+
+                  <button className='btn flex'>
+                  <Link to="/login">
+                    BASHKOHU TANI <HiOutlineClipboardCheck className='icon' />
+                  </Link>
+                    {/*<HiOutlineClipboardCheck className='icon' /> */}
+                  </button>
+                </div>
+              </div>
+            )
+            
+          })
+        }
+
+      </div>
     
     </section>
     <Footer/>
