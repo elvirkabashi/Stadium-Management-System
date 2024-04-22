be43dbf31ef2a976df30821a710e644ff02096e5
Rigon Pira <rp57421@ubt-uni.net>
2024-04-22 22:12:02 +0200

    Admini mund te shtoj fans category nga CRUD

--- frontend/stadium_ui/src/pages/User/Fans/Fans.jsx
+++ frontend/stadium_ui/src/pages/User/Fans/Fans.jsx
@@ -1,91 +1,57 @@
-import {useEffect} from 'react'
-import {HiOutlineClipboardCheck} from 'react-icons/hi'
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { HiOutlineLocationMarker, HiOutlineClipboardCheck } from 'react-icons/hi';
+import Footer from '../../../components/Footer/Footer';
+import { Link } from 'react-router-dom';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+import axios from 'axios';
 
-//import the images 
-import KosovoStandard from '../../../Assets/KosovoStandard.jpg'
-import KosovoPremium from '../../../Assets/KosovoPremium.jpg'
-import KosovoJunior from '../../../Assets/KosovoJunior.jpg'
+// Import the images
+import KosovoStandard from '../../../Assets/KosovoStandard.jpg';
+import KosovoPremium from '../../../Assets/KosovoPremium.jpg';
+import KosovoJunior from '../../../Assets/KosovoJunior.jpg';
 import './Fans.css';
 
-import Aos from 'aos'
-import 'aos/dist/aos.css'
-
-//array named Data
-const Data = [
-    
-  {
-    id:1,
-    imgSrc:KosovoPremium,
-    title:'Kosovo Premium Fan',
-    titleDescription: 'Actual Premium Kosovar membership card.',
-    priceDescription: 'Only for',
-    price: '35€',
-    description: '15% discount at our Offical Online Shop, Advance Ticket Purchase, All of the benefits of the other plans.'
-  },
-
-  {
-    id:2,
-    imgSrc:KosovoStandard,
-    title:'Kosovo Standard Fan',
-    titleDescription: 'Actual Kosovar digital card.',
-    priceDescription: 'Free',
-    price: '0€',
-    description: '5% discount on your first purchase on the Official Online Store, Contests and giveaways.'
-  },
-
-  {
-    id:3,
-    imgSrc:KosovoJunior,
-    title:'Kosova Junior Fan',
-    titleDescription: 'Actual Junior Kosovar membership card.',
-    priceDescription: 'Only for',
-    price: '20€',
-    description: 'Advance Ticket Purchase, Yearly surprise gift.'
-  },
-
-]
-
-
-
 const Fans = () => {
-  
-  //create a react hook to add scroll animation
+  const [fansData, setFansData] = useState([]);
+
   useEffect(() => {
-    Aos.init({duration: 2000})
-  },[])
+    Aos.init({ duration: 2000 });
+    fetchData();
+  }, []);
 
-
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:5163/api/Fans');
+      setFansData(response.data);
+    } catch (error) {
+      console.error('Error fetching fans:', error);
+    }
+  };
 
   return (
-<>
-<div className="events">
-          <h2 data-aos='fade-right'>FANS</h2>
-        </div>
-    <section className='new-main container section'>
-
-      <div className="new-secTitle">
-        <h3 data-aos="fade-right"  className="title">
-          BASHKOHU ME KOMBETAREN
-        </h3>
+    <>
+      <div class="events">
+        <h2 data-aos="fade-right">FANS</h2>
       </div>
-      <div className="new-secContent grid">
-        
-        {
-          Data.map(({id, imgSrc, title, titleDescription, priceDescription, price, description})=>{
-            return( 
+      <section className="new-main container section">
+        <div className="new-secTitle">
+          <h3 data-aos="fade-right" className="title">
+            BASHKOHU ME KOMBETAREN
+          </h3>
+        </div>
+        <div className="new-secContent grid">
+          {fansData.map(({ id, imageUrl, title, titleDescription, priceDescription, price, description, }) => {
+            return (
               <div key={id} className="singleDestination" data-aos="fade-up">
-                
                 <div className="imageDiv">
-                  <img src={imgSrc} alt={title} />
+                  <img src={imageUrl} alt={title} />
                 </div>
-
                 <div className="cardInfo">
                   <h4 className="destTitle">{title}</h4>
                   <span className="continent flex">
-                     <span className="name">{titleDescription}</span>
+                    <span className="name">{titleDescription}</span>
                   </span>
-
                   <div className="fees flex">
                     <div className="grade">
                       <span>{priceDescription}</span>
@@ -93,31 +59,24 @@ const Fans = () => {
                     <div className="price">
                       <span>{price}</span>
                     </div>
- 
                   </div>
-
                   <div className="desc">
                     <p>{description}</p>
                   </div>
-
-                  <button className='btn flex'>
-                  <Link to="/login">
-                    BASHKOHU TANI <HiOutlineClipboardCheck className='icon' />
-                  </Link>
-                    {/*<HiOutlineClipboardCheck className='icon' /> */}
+                  <button className="btn flex">
+                    <Link to="/login">
+                      BASHKOHU TANI <HiOutlineClipboardCheck className="icon" />
+                    </Link>
                   </button>
                 </div>
               </div>
-            )
-            
-          })
-        }
-
-      </div>
-    
-    </section>
+            );
+          })}
+        </div>
+      </section>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Fans
+export default Fans;
