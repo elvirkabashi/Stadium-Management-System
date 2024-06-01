b21a315ce5b913f74022bd41a8045c8eef8573de
Rigon Pira <rp57421@ubt-uni.net>
2024-06-01 11:23:25 +0200

    Ndryshimi tek main per arsye te errorit

--- frontend/stadium_ui/src/main.jsx
+++ frontend/stadium_ui/src/main.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import { ChakraProvider, theme } from '@chakra-ui/react'
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import App from './App'
+// import { ChakraProvider, theme } from '@chakra-ui/react'
 
-ReactDOM.render(
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <ChakraProvider theme={theme}>
+//       <App />
+//     </ChakraProvider>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// )
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
+ document.getElementById('root')
 )
